89acfda88ad7b6c3181e7f3b26d5f8ae4a902dcf
FirSae <firouzs@hotmail.com>
2018-05-18 19:40:55 +0300

    add text

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 def generate_buzz():
 
-    page = '<html><body><h1>'
+    page = '<html><body><p>This is some text in a paragraph for test.</p><h1>'
 
     page += generator.generate_buzz()
 
-    page += '</h1> <p>This is some text in a paragraph for test.</p> </body></html>'
+    page += '</h1>  </body></html>'
 
     return page
 
